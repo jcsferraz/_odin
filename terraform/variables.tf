825a93ab6e3a8b5b5d0bba2bbdfd15598afce1b4
julio.ferraz <julio.ferraz@gmail.com>
2022-07-09 20:24:16 -0300

    envio de dados para repositorio v11

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,20 +5,20 @@ variable "aws_region" {
 }
 variable "vpc_cidr_block" {
   description = "The top-level CIDR block for the VPC."
-  default     = "11.1.0.0/20"
+  default     = "11.0.0.0/20"
 }
 
 variable "cidr_blocks" {
   description = "The CIDR blocks to create the workstations in."
-  default     = ["11.1.1.0/23", "11.2.1.0/23"]
+  default     = ["11.2.0.0/23", "11.4.0.0/23","11.6.0.0/23","11.8.0.0/23","11.10.0.0/23","11.12.0.0/23"]
 }
 variable "public_subnets" {
   description = "The CIDR blocks to create the workstations in."
-  default     = ["11.1.1.0/23", "11.2.1.0/23"]
+  default     = ["11.2.0.0/23", "11.4.0.0/23","11.6.0.0/23"]
   
 }
 variable "private_subnets" {
   description = "The CIDR blocks to create the workstations in."
-  default     = ["11.1.1.0/23", "11.2.1.0/23"]
+  default     = ["11.8.0.0/23", "11.10.0.0/23","11.12.0.0/23"]
   
 }
