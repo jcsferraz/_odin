4bb4e068843ddb10a047a39b02fb8f595b7aefaf
julio.ferraz <julio.ferraz@gmail.com>
2022-07-10 14:03:07 -0300

    envio de dados para repositorios v34

--- terraform/account/vops-cloud/services/openshift-community/envs/dev/nodes/src/install_openshift-community-nodes.sh
+++ terraform/account/vops-cloud/services/openshift-community/envs/dev/nodes/src/install_openshift-community-nodes.sh
@@ -12,7 +12,7 @@ echo "Install git/wget and curl"
 yum install -y git wget curl --nogpgcheck
 
 echo "Install git/wget/curl and CRI-O"
-yum install -y git wget curl --nogpgcheck
+yum install -y git wget curl pyOpenSSL httpd-tools python3-setuptools python2-setuptools --nogpgcheck
 yum -y install cri-o --nogpgcheck
 
 echo "Install aws ssm agent "
@@ -35,15 +35,25 @@ curl -O https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshi
 tar -xvzf openshift-client-linux.tar.gz
 cp -rf oc /usr/local/bin/oc ; chmod +x /usr/local/bin/oc ; cp -rf kubectl /usr/local/bin/kubectl; chmod +x /usr/local/bin/kubectl
 
-echo "Setting Openshift"
+echo "Setting Openshift 3.11"
 
-curl -O https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-install-linux.tar.gz
-tar -xvzf openshift-install-linux.tar.gz
-cp -rf openshift-install /usr/local/bin/openshift-install ; chmod +x /usr/local/bin/openshift-install
-mkdir -p /opt/oc-community-cluster
-openshift-install create install-config --dir=/opt/oc-community-cluster
-openshift-install create manifests --dir=/opt/oc-community-cluster
-rm -f /opt/oc-community-cluster/openshift/99_openshift-cluster-api_master-machines-*.yaml
-rm -f /opt/oc-community-cluster/openshift/99_openshift-cluster-api_worker-machineset-*.yaml
-openshift-install create ignition-configs --dir=/opt/oc-community-cluster
-openshift-install wait-for bootstrap-complete --dir=/opt/oc-community-cluster --log-level=info 
+git clone -b release-3.11 --single-branch https://github.com/openshift/openshift-ansible /usr/share/openshift-ansible
+cd /usr/share/openshift-ansible
+sed -i 's/openshift.common.ip/openshift.common.public_ip/' roles/openshift_control_plane/templates/master.yaml.v1.j2
+
+ansible-playbook /usr/share/openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook /usr/share/openshift-ansible/playbooks/deploy_cluster.yml
+
+htpasswd -Bbc /etc/origin/master/htpasswd vops vops
+
+
+#curl -O https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-install-linux.tar.gz
+#tar -xvzf openshift-install-linux.tar.gz
+#cp -rf openshift-install /usr/local/bin/openshift-install ; chmod +x /usr/local/bin/openshift-install
+#mkdir -p /opt/oc-community-cluster
+#openshift-install create install-config --dir=/opt/oc-community-cluster
+#openshift-install create manifests --dir=/opt/oc-community-cluster
+#rm -f /opt/oc-community-cluster/openshift/99_openshift-cluster-api_master-machines-*.yaml
+#rm -f /opt/oc-community-cluster/openshift/99_openshift-cluster-api_worker-machineset-*.yaml
+#openshift-install create ignition-configs --dir=/opt/oc-community-cluster
+#openshift-install wait-for bootstrap-complete --dir=/opt/oc-community-cluster --log-level=info 
