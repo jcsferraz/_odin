8a5aabb7d46dbe27e465ff9b8e95da234184a3ea
julio.ferraz <julio.ferraz@gmail.com>
2022-08-17 22:00:15 -0300

    envio de dados para repositorio v41

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,27 +11,37 @@ variable "vpc" {
 
 variable "vpc_cidr_block" {
   description = "The top-level CIDR block for the VPC."
-  default     = "11.0.0.0/16"
+  default     = "11.0.0.0/20"
 }
 
 variable "secondary_vpc_cidr_block" {
   description = "The top-level Secondary CIDR block for the VPC."
-  default     = "12.0.0.0/16"
+  default     = "12.0.0.0/20"
 }
 
 variable "cidr_blocks" {
   description = "The CIDR blocks to create the workstations in."
-  default     = ["11.0.2.0/23", "11.0.4.0/23", "11.0.6.0/23", "11.0.8.0/23", "11.0.10.0/23", "11.0.12.0/23", "12.0.2.0/23", "12.0.4.0/23", "12.0.6.0/23", "12.0.8.0/23", "12.0.10.0/23", "12.0.12.0/23"]
+  default     = ["11.0.2.0/23", "11.0.4.0/23", "11.0.6.0/23", "11.0.8.0/23", "11.0.10.0/23", "11.0.12.0/23", "12.0.0.0/23", "12.0.2.0/23", "12.0.4.0/23", "12.0.6.0/23", "12.0.8.0/23", "12.0.10.0/23"]
 }
+
+# variable "public_subnets" {
+#  description = "The CIDR blocks to create the workstations in."
+#  default     = ["11.0.2.0/23", "11.0.4.0/23", "11.0.6.0/23", "12.0.0.0/23", "12.0.2.0/23", "12.0.4.0/23"]
+#  
+#}
 
 variable "public_subnets" {
-  description = "The CIDR blocks to create the workstations in."
-  default     = ["11.0.2.0/23", "11.0.4.0/23", "11.0.6.0/23", "12.0.2.0/23", "12.0.4.0/23", "12.0.6.0/23"]
-  
+  type        = string
+  default     = null
 }
 
+# variable "private_subnets" {
+#  description = "The CIDR blocks to create the workstations in."
+#  default     = ["11.0.8.0/23", "11.0.10.0/23", "11.0.12.0/23", "12.0.6.0/23", "12.0.8.0/23", "12.0.10.0/23"]
+#  
+#}
+
 variable "private_subnets" {
-  description = "The CIDR blocks to create the workstations in."
-  default     = ["11.0.8.0/23", "11.0.10.0/23", "11.0.12.0/23", "12.0.8.0/23", "12.0.10.0/23", "12.0.12.0/23"]
-  
+  type        = string
+  default     = null
 }
